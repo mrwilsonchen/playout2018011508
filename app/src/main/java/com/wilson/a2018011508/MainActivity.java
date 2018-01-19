64cf7a24fa98f50f838d51bde2d323f28bad43ee
C1 <c1@gjun.com>
2018-01-19 11:52:47 +0800

    2

--- app/src/main/java/com/wilson/a2018011508/MainActivity.java
+++ app/src/main/java/com/wilson/a2018011508/MainActivity.java
@@ -1,28 +1,123 @@
 package com.wilson.a2018011508;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.util.DisplayMetrics;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    //宣告
+    private ImageView mImg;
+    private DisplayMetrics mPhone;
+    private final static int CAMERA = 66 ;
+    private final static int PHOTO = 99 ;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         setContentView(R.layout.main);
+
+        //讀取手機解析度
+        mPhone = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(mPhone);
+
+        mImg = (ImageView) findViewById(R.id.img);
+        Button mCamera = (Button) findViewById(R.id.camera);
+        Button mPhoto = (Button) findViewById(R.id.photo);
+
+        mCamera.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //開啟相機功能，並將拍照後的圖片存入SD卡相片集內，須由startActivityForResult且
+                //帶入requestCode進行呼叫，原因為拍照完畢後返回程式後則呼叫onActivityResult
+                ContentValues value = new ContentValues();
+                value.put(Media.MIME_TYPE, "image/jpeg");
+                Uri uri= getContentResolver().insert(Media.EXTERNAL_CONTENT_URI,
+                        value);
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri.getPath());
+                startActivityForResult(intent, CAMERA);
+            }
+        });
+
+        mPhoto.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因
+                //為點選相片後返回程式呼叫onActivityResult
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, PHOTO);
+            }
+        });
     }
-    public void click1(View v)
+
+    //拍照完畢或選取圖片後呼叫此函式
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data)
     {
-        CameraActivity ca=new CameraActivity();
+        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
+        if ((requestCode == CAMERA || requestCode == PHOTO ) && data != null)
+        {
+            //取得照片路徑uri
+            Uri uri = data.getData();
+            ContentResolver cr = this.getContentResolver();
 
+            try
+            {
+                //讀取照片，型態為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+
+                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
+                if(bitmap.getWidth()>bitmap.getHeight())ScalePic(bitmap,
+                        mPhone.heightPixels);
+                else ScalePic(bitmap,mPhone.widthPixels);
+            }
+            catch (FileNotFoundException e)
+            {
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
-    public void click2(View v)
+
+    private void ScalePic(Bitmap bitmap,int phone)
     {
+        //縮放比例預設為1
+        float mScale = 1 ;
 
+        //如果圖片寬度大於手機寬度則進行縮放，否則直接將圖片放入ImageView內
+        if(bitmap.getWidth() > phone )
+        {
+            //判斷縮放比例
+            mScale = (float)phone/(float)bitmap.getWidth();
+
+            Matrix mMat = new Matrix() ;
+            mMat.setScale(mScale, mScale);
+
+            Bitmap mScaleBitmap = Bitmap.createBitmap(bitmap,
+                    0,
+                    0,
+                    bitmap.getWidth(),
+                    bitmap.getHeight(),
+                    mMat,
+                    false);
+            mImg.setImageBitmap(mScaleBitmap);
+        }
+        else mImg.setImageBitmap(bitmap);
     }
-
 }
