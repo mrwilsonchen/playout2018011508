0b2e15dbdc1cc1be6b1377eb198aa31b6d00996c
C1 <c1@gjun.com>
2018-01-19 14:40:27 +0800

    2

--- app/src/main/java/com/wilson/a2018011508/MainActivity.java
+++ app/src/main/java/com/wilson/a2018011508/MainActivity.java
@@ -1,19 +1,32 @@
 package com.wilson.a2018011508;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.provider.MediaStore.Images.Media;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.util.DisplayMetrics;
+
+import java.io.FileNotFoundException;
+
+//import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity
 {
+
     //宣告
     private ImageView mImg;
     private DisplayMetrics mPhone;
@@ -24,7 +37,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         //讀取手機解析度
         mPhone = new DisplayMetrics();
@@ -39,8 +52,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                //開啟相機功能，並將拍照後的圖片存入SD卡相片集內，須由startActivityForResult且
-                //帶入requestCode進行呼叫，原因為拍照完畢後返回程式後則呼叫onActivityResult
+                //開啟相機功能，並將拍照後的圖片存入SD卡相片集內，須由startActivityForResult且帶入requestCode進行呼叫，原因為拍照完畢後返回程式後則呼叫onActivityResult
                 ContentValues value = new ContentValues();
                 value.put(Media.MIME_TYPE, "image/jpeg");
                 Uri uri= getContentResolver().insert(Media.EXTERNAL_CONTENT_URI,
@@ -56,8 +68,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                //開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因
-                //為點選相片後返回程式呼叫onActivityResult
+                //開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因為點選相片後返回程式呼叫onActivityResult
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
